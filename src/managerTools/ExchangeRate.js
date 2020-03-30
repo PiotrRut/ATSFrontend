6d0a9d02292121675fa688042397fe386f20ede4
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-03-30 14:51:35 +0200

    RegEx validation on exchange rate

--- src/managerTools/ExchangeRate.js
+++ src/managerTools/ExchangeRate.js
@@ -16,39 +16,62 @@ class ExchangeRate extends React.Component {
 
     this.state = {
       exchangeRate: { },
-      updatedRate: ''
+      updatedRate: '',
+      helperText: '',
+      error: false
     }
   } 
 
+  // Get the exchange rate on component mount and set state
   componentDidMount() {
     axios.get(`${APIURL}/sales/getExchangeRate?secret_token=${this.props.token}`)
     .then(res => this.setState({ exchangeRate: res.data }, console.log(res)))
   }
 
+  // Post the updated changes to the database
   updateExchangeRate = () => {
-      axios.post(`${APIURL}/sales/updateExchangeRate?secret_token=${this.props.token}`, {
-          _id: this.state.exchangeRate._id,
-          rate: this.state.updatedRate
+      axios.patch(`${APIURL}/sales/updateExchangeRate?secret_token=${this.props.token}`, {
+          '_id': this.state.exchangeRate._id,
+          'rate': this.state.updatedRate
       }).then(res => {
         window.location.reload()
       })
   }
 
+  // Updating state and validating input from the text field
   handleInput = (e) => {
     this.setState({ updatedRate: e.target.value })
+    if (e.target.value.match(/^[0-9]{5}\.[0-9]{4}$/)) {
+      this.setState({ helperText: '', error: false })
+    } else {
+      this.setState({ helperText: 'Format must match XXXXX.YYYY', error: true })
+    }
   }
 
   render () {
     return (
     <div>
-    <h1>The current exchange rate is:</h1>
+    <h1>The current local currency exchange rate is:</h1>
     <h1>$1 USD * {this.state.exchangeRate.rate} = £1 ({this.state.exchangeRate.localCurrencyCode})</h1>
     <h5>Last Update: {moment(this.state.exchangeRate.updatedAt).format('MMMM Do YYYY, h:mm:ss a')}</h5>
     <Grid container spacing={0} direction="column" alignItems="center" justify="center" >
         <Grid item xs={3}>
-            <TextField id="standard-basic" label="New Exchange Rate" value={this.state.updatedRate} onChange={this.handleInput}/>
+            <TextField
+             id="standard-basic" 
+             label="New Exchange Rate" 
+             value={this.state.updatedRate} 
+             onChange={this.handleInput}
+             error={this.state.error}
+             helperText={this.state.helperText}
+             style={{width: '250px'}}
+             />
             <br/> <br/> 
-            <Button onClick={this.updateExchangeRate} startIcon={<AttachMoneyRoundedIcon/>}>Update Exchange Rate</Button>
+            <Button
+             disabled={this.state.error} 
+             onClick={this.updateExchangeRate} 
+             startIcon={<AttachMoneyRoundedIcon/>}>
+               Update Exchange Rate
+            </Button>
         </Grid>
     </Grid>
     </div>
